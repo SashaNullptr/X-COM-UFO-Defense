5ccb622ddbc9350550a9addc3ce81cf11524d723
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-07-03 13:57:29 -0600

    Added DB config file and parsing function

--- x-com-ufo-defense/__init__.py
+++ x-com-ufo-defense/__init__.py
@@ -1,3 +1,5 @@
 import os
 
 ROOT_DIR = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+CONFIG_DIR = os.path.join( ROOT_DIR, 'config_files' )
+DB_CONFIG = os.path.join( CONFIG_DIR, 'database.ini' )
