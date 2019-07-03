31b99da9c28053e8412a37bc0fedde25e41c4b41
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-07-03 13:58:06 -0600

    Sketch of counter function

--- x-com-ufo-defense/counter.py
+++ x-com-ufo-defense/counter.py
@@ -1,1 +1,10 @@
 import psycog2
+
+def count_rows_in_col():
+
+    count_command = 'SELECT COUNT(*) from table'
+    cur.execute(count_command)
+    results = cur.fetchone()
+    number_of_rows = result[0]
+
+    return number_of_rows
