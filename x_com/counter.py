f424fad359a562b047a7903d10ad4fa9b69acb20
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-07-04 16:53:08 -0600

    Use send query instead of raw commands

--- x_com/counter.py
+++ x_com/counter.py
@@ -1,10 +1,10 @@
 import psycog2
+from x_com.db_query import send_query
 
 def count_rows_in_col():
 
     count_command = 'SELECT COUNT(*) from table'
-    cur.execute(count_command)
-    results = cur.fetchone()
+    results = send_query(counter_query)
     number_of_rows = result[0]
 
     return number_of_rows
