4a1144d6191239627949c431ab54e6f87d688779
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-07-04 14:40:04 -0600

    Changed table name

--- data_munging_tools/csv_to_SQL.py
+++ data_munging_tools/csv_to_SQL.py
@@ -29,7 +29,7 @@ def csv_to_SQL( csv_file ):
   with open (csv_file, 'r') as f:
     reader = csv.reader(f)
     columns = next(reader)
-    query = 'insert into MyTable({0}) values ({1})'
+    query = 'insert into ufo_data({0}) values ({1})'
     query = query.format(','.join(columns), ','.join('?' * len(columns)))
     cursor = connection.cursor()
     for data in reader:
