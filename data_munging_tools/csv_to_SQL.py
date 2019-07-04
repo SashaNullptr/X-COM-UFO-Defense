80c3c535fc1d5cba78b0f9b5004db2b59bb14f00
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-07-04 14:39:09 -0600

    Blocked out create table function.

--- data_munging_tools/csv_to_SQL.py
+++ data_munging_tools/csv_to_SQL.py
@@ -1,6 +1,29 @@
 import csv, psycog2
 import sys
 
+from x_com import send_query_no_results
+
+def create_ufo_data_table():
+    """ create tables in the PostgreSQL database"""
+    # id,occurred_at,city,state,country,shape,duration_seconds,duration_text,description,reported_on,latitude,longitude
+    create_table_command =
+        """
+        CREATE TABLE ufo_data (
+            id SERIAL,
+            occurred_at TIMESTAMP,
+            city TEXT,
+            state CHAR(2),
+            country CHAR(2),
+            shape TEXT,
+            duration_seconds INTEGER,
+            duration_text TEXT,
+            reported_on DATE,
+            latitude FLOAT8,
+            longitude FLOAT8
+        )
+        """
+        send_query_no_results(create_table_command)
+
 def csv_to_SQL( csv_file ):
   with open (csv_file, 'r') as f:
     reader = csv.reader(f)
