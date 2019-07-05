b6202edbb8265e40c0f4289b00eca0edf0e2ca41
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-07-05 01:04:51 -0600

    Final revision of counter function.

--- x_com/counter.py
+++ x_com/counter.py
@@ -1,10 +1,10 @@
-import psycog2
 from x_com.db_query import send_query
 
-def count_rows_in_col():
 
-    count_command = 'SELECT COUNT(*) from table'
-    results = send_query(counter_query)
+def count_rows_in_table():
+
+    count_command = 'SELECT COUNT(*) from ufo_data'
+    result = send_query(count_command)
     number_of_rows = result[0]
 
-    return number_of_rows
+    return { "count" : number_of_rows }
