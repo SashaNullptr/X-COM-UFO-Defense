f5796affcebe6dfb4ffc9de1ebe176db3198f8a0
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-07-05 01:02:55 -0600

    Blocked out total sighting API wrapper.

--- x_com_app.py
+++ x_com_app.py
@@ -1,10 +1,43 @@
-From flask import Flask
+# System
+import os
+import json
+import logging
+from urllib import parse
+
+# Third Party
+from flask import Flask, request, json, redirect, url_for, send_from_directory
+from flask_cors import CORS, cross_origin
+
+# Local
+from x_com import count_rows_in_table
+
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello_world:
-    return 'Hey, we have Flask in a Docker container!'
+@app.route('/counter', methods=['GET'])
+def get_answer( event=None, context=None ):
+    """
+    API endpoint that returns the total number of UFO sightings.
 
+    An example request might look like:
+
+    .. sourcecode:: http
+
+       GET www.x.com/counter HTTP/1.1
+       Host: example.com
+       Accept: application/json, text/javascript
+
+    Results will be returned as JSON object with the following format:
+
+    .. code-block:: json
+
+        {
+          "count": <number>
+        }
+
+    """
+
+    return app.response_class( json.dumps( count_rows_in_table() ), mimetype='application/json' )
 
 if __name == '__main__':
     app.run(debug=True, host='0.0.0.0')
