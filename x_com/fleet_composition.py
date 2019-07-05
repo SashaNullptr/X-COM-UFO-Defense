cac6a3405de4c0425762c2953fcf4163d90b770f
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-07-05 13:05:10 -0600

    Return count of unique shapes instead of raw shapes.

--- x_com/fleet_composition.py
+++ x_com/fleet_composition.py
@@ -4,7 +4,7 @@ from x_com.db_query import send_query_fetchone
 def unique_ship_shapes():
 
     # Get number of unique shapes where shape entry is non-null.
-    unique_shapes_query = 'SELECT * FROM (SELECT DISTINCT shape FROM ufo_data) AS temp WHERE shape <> '';'
+    unique_shapes_query = "SELECT COUNT(*) FROM (SELECT DISTINCT shape FROM ufo_data) AS temp WHERE shape <> '';"
     result = send_query_fetchone(unique_shapes_query)
     unique_shapes = result[0]
 
